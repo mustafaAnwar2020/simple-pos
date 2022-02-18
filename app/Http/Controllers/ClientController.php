3e22b49b4cb7fd8685b7ccd545c41034f8bebb2b
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-18 19:15:38 +0200

    Create ClientController.php

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $clients = Client::when($request->search,function($q) use ($request){
+            return $q->where('name','like','%' . $request->search . '%')
+            ->orwhere('phone','like','%' . $request->search . '%')
+            ->orwhere('address','like','%' . $request->search . '%');
+        })->latest()->paginate(5);
+        return view('dashboard.clients.index')->with('clients',$clients);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard.clients.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|string',
+            'email' =>'required|email',
+            'address' => 'required',
+            'phone'=> 'required'
+        ]);
+        $client = Client::create($request->all());
+        return redirect()->route('dashboard.client.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        return view('dashboard.clients.show')->with('client',$client);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Client $client)
+    {
+        return view('dashboard.clients.edit')->with('client',$client);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        $this->validate($request,[
+            'name'=>'required|string',
+            'email' =>'required|email',
+            'address' => 'required',
+            'phone'=> 'required'
+        ]);
+        $client->update($request->all());
+        return redirect()->route('dashboard.client.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $client->destroy($client->id);
+        return redirect()->route('dashboard.client.index');
+    }
+}
