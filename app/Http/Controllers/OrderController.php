cbd29208abe8d58eaca9c09728569c25e0fe9f99
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-21 05:53:32 +0200

    Create OrderController.php

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\Client;
+use App\Models\category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orders = Order::all();
+        return view('dashboard.orders.index')->with('orders',$orders);
+    }
+
+    public function create(Client $client)
+    {
+        $categories = category::all();
+        return view('dashboard.clients.orders.create')->with('categories',$categories)->with('client',$client);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,Client $client)
+    {
+        $this->validate($request,[
+            'products'=>'required|array',
+        ]);
+        $order = $client->orders()->create([]);
+        $order->products()->attach($request->products);
+        $totalPrice=0;
+        foreach($request->products as $id=>$quantity)
+        {
+            $product = Product::find($id);
+            $totalPrice += $product->sale_price * $quantity['quantity'];
+
+            $product->update([
+                'stock' =>$product->stock - $quantity['quantity'],
+            ]);
+        }
+
+        $order->update([
+            'total_price'=>$totalPrice,
+        ]);
+        return redirect()->route('dashboard.orders.index');
+    }
+
+    public function edit(Client $client,Order $order)
+    {
+        $categories = category::with('products')->get();
+        $orders =$client->orders()->with('products')->paginate(5);
+        return view('dashboard.clients.orders.edit')->with('client',$client)->with('categories',$categories)->with('orders',$orders)->with('order',$order);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Order $order)
+    {
+        //
+    }
+
+    public function products(Order $order){
+        $products=$order->products;
+        return view('dashboard.orders.products')->with('products',$products)->with('order',$order);
+    }
+    public function destroy(Order $order)
+    {
+        $order->delete($order->id);
+        return redirect()->route('dashboard.orders.index');
+    }
+}
