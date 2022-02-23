9d2b54cb33863422b85ede9c7a614453c0c4a7aa
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-23 04:18:59 +0200

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\category;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Client;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard.index');
+        $category_count = category::count();
+        $Order_count = Order::count();
+        $Product_count = Product::count();
+        $Client_count = Client::count();
+
+        return view('dashboard.index')->with('cat_count',$category_count)->with('order_count',$Order_count)
+        ->with('product_count',$Product_count)->with('client_count',$Client_count);
     }
 }
