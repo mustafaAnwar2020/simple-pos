73c5750ae390856c92e3c25b3e94537abd990b3e
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-23 05:56:55 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the user associated with the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function profile()
+    {
+        return $this->hasOne('App\Models\Profile');
+    }
 }
