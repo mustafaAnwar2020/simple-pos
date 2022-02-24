b0488df896fe7e0c058e8d31dd1d2f665c5050e1
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-24 03:52:11 +0200

    add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -22,23 +22,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'App\Http\Controllers\DashboardController@index')->name('home');
-
-Route::group(['prefix' => LaravelLocalization::setLocale()], function()
-{
-	Route::prefix('dashboard')->name('dashboard.')->group(function () {
-        Route::get('/index','App\Http\Controllers\DashboardController@index');
-        Route::resource('category','App\Http\Controllers\CategoryController');
-        Route::resource('product','App\Http\Controllers\ProductController');
-        Route::get('/client/order/create/{client}','App\Http\Controllers\OrderController@create')->name('order.create');
-        Route::post('/client/order/create/{client}','App\Http\Controllers\OrderController@store')->name('order.store');
-        Route::get('/client/{client}/orders/edit/{order}','App\Http\Controllers\OrderController@edit')->name('order.edit');
-        Route::put('/client/{client}/orders/edit/{order}','App\Http\Controllers\OrderController@update')->name('order.update');
-        Route::resource('client','App\Http\Controllers\ClientController');
-        Route::get('/orders/products/{order}','App\Http\Controllers\OrderController@products')->name('orders.products');
-        Route::resource('orders','App\Http\Controllers\OrderController');
-        Route::get('/profile','App\Http\Controllers\ProfileController@index')->name('profile.index');
-        Route::put('/profile/{profile}','App\Http\Controllers\ProfileController@update')->name('profile.update');
-        Route::post('/profile/','App\Http\Controllers\ProfileController@changePassword')->name('profile.password');
-        Route::resource('user', 'App\Http\Controllers\UsersController');
+Route::middleware('auth')->group(function () {
+    Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+        Route::prefix('dashboard')->name('dashboard.')->group(function () {
+            Route::get('/index', 'App\Http\Controllers\DashboardController@index');
+            Route::resource('category', 'App\Http\Controllers\CategoryController');
+            Route::resource('product', 'App\Http\Controllers\ProductController');
+            Route::get('/client/order/create/{client}', 'App\Http\Controllers\OrderController@create')->name('order.create');
+            Route::post('/client/order/create/{client}', 'App\Http\Controllers\OrderController@store')->name('order.store');
+            Route::get('/client/{client}/orders/edit/{order}', 'App\Http\Controllers\OrderController@edit')->name('order.edit');
+            Route::put('/client/{client}/orders/edit/{order}', 'App\Http\Controllers\OrderController@update')->name('order.update');
+            Route::resource('client', 'App\Http\Controllers\ClientController');
+            Route::get('/orders/products/{order}', 'App\Http\Controllers\OrderController@products')->name('orders.products');
+            Route::resource('orders', 'App\Http\Controllers\OrderController');
+            Route::get('/profile', 'App\Http\Controllers\ProfileController@index')->name('profile.index');
+            Route::put('/profile/{profile}', 'App\Http\Controllers\ProfileController@update')->name('profile.update');
+            Route::post('/profile/', 'App\Http\Controllers\ProfileController@changePassword')->name('profile.password');
+            Route::resource('user', 'App\Http\Controllers\UsersController');
+        });
     });
 });
