c987666c0ea2ea90855575861b4b50b73f05d17a
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-28 08:34:50 +0200

    Update create_product_order_table.php

--- database/migrations/create_product_order_table.php
+++ database/migrations/create_product_order_table.php
@@ -22,7 +22,6 @@ class CreateProductOrderTable extends Migration
 
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
-            $table->timestamps();
         });
     }
 
