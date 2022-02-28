1e697fb922fecc862b1148a454b0e1ae6938bef4
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-28 08:34:20 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:passport')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('login','App\Http\Controllers\API\LoginController@login');
+Route::post('register','App\Http\Controllers\API\RegisterController@register');
+Route::middleware('auth:api')->group(function(){
+    Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+        Route::prefix('dashboard')->name('dashboard.')->group(function () {
+            Route::apiResource('/categories','App\Http\Controllers\API\CategoryController');
+            Route::apiResource('/products','App\Http\Controllers\API\ProductController');
+            Route::put('/Order/{client}/update/{order}','App\Http\Controllers\API\OrderController@update');
+            Route::post('/Order/store/{client}','App\Http\Controllers\API\OrderController@store');
+            Route::apiResource('/Order','App\Http\Controllers\API\OrderController');
+            Route::apiResource('/clients','App\Http\Controllers\API\ClientController');
+        });
+
+    });
 });
