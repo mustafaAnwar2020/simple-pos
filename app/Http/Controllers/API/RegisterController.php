096a0d91afbecedea74f7b583f99a0c9ce0437ba
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-28 06:54:16 +0200

    Create RegisterController.php

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\API\BaseController as Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request){
+        $input = $request->all();
+        $validator = Validator::make($input,[
+            'name' =>"required|string",
+            'email' => 'required|email|max:255',
+            'password' =>'required|string|min:8',
+            'c_password' => 'required|string|min:8|same:password'
+        ]);
+        if($validator->fails())
+        {
+            return $this->sendError($validator->errors(),'someting is wrong!');
+        }
+        $input['password'] = Hash::make($input['password']);
+        $user = User::create($input);
+        // $user->assignRole('user');
+        $success['token'] = $user->createToken('123123asd')->accessToken;
+        return $this->sendResponse($success,'user created successfully!');
+    }
+}
