4a6e376ad8a483442860e6ab1f08d6ef23b8b1ac
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-28 06:56:26 +0200

    Create productRresource.php

---
+++ app/Http/Resources/productRresource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class productRresource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'sale_price'=>$this->sale_price,
+            'purchase_price'=>$this->purchase_price,
+            'name'=>$this->name,
+            'description'=>$this->description,
+            'created_at'=>$this->created_at,
+            'updated_at'=>$this->updated_at,
+        ];;
+    }
+}
