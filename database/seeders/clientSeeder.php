174f42d7a063fdc1a03a21d07ca8786af55c3542
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-03-01 01:18:04 +0200

    Create clientSeeder.php

---
+++ database/seeders/clientSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Client;
+class clientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Client::factory()->count(30)->create();
+    }
+}
