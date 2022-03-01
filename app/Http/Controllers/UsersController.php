2b0564c5d5638d955409fa80c2d87ca3a09d6d11
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-03-01 02:15:27 +0200

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,9 +6,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Models\Profile;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class UsersController extends Controller
 {
+
+    function __construct()
+    {
+        $this->middleware('permission:user-list|user-create|user-edit|user-delete', ['only' => ['index', 'store']]);
+        $this->middleware('permission:user-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:user-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:user-delete', ['only' => ['destroy']]);
+    }
+
     public function index(){
 
         $user = User::paginate(20);
@@ -16,8 +27,8 @@ class UsersController extends Controller
     }
 
     public function create(){
-        // $role=Role::all();
-        return view('dashboard.users.create');
+        $role=Role::all();
+        return view('dashboard.users.create')->with('role',$role);
     }
 
     public function store(Request $request)
@@ -29,8 +40,8 @@ class UsersController extends Controller
             'phone' => 'required|string',
             'contact' => 'required|url',
             'password' =>'required|string|min:8',
+            'role'=>'required'
         ]);
-        // dd($request);
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -43,15 +54,14 @@ class UsersController extends Controller
             'phone'=>$request->phone,
             'contact'=>$request->contact
         ]);
-        // $user->assignRole($request->role);
+        $user->assignRole($request->role);
         return redirect()->route('dashboard.user.index');
     }
 
     public function edit(User $user){
-        // $role=Role::all();
-        // $userRole = DB::table('model_has_roles')->where('model_id',$user->id)->pluck('role_id','role_id')->first();
-        // dd($userRole);
-        return view('dashboard.users.edit')->with('user',$user);
+        $role=Role::all();
+        $userRole = DB::table('model_has_roles')->where('model_id',$user->id)->pluck('role_id','role_id')->first();
+        return view('dashboard.users.edit')->with('user',$user)->with('role',$role)->with('userRole',$userRole);
     }
 
     public function update(Request $request, User $user){
@@ -61,7 +71,7 @@ class UsersController extends Controller
             'address' => 'required|string',
             'phone' => 'required|string',
             'contact' => 'required|url',
-            // 'role' =>'required'
+            'role' =>'required'
         ]);
         $user->name = $request->name;
         $user->email = $request->email;
@@ -70,8 +80,8 @@ class UsersController extends Controller
         $user->profile->address = $request->address;
         $user->profile->contact = $request->contact;
         $user->profile->save();
-        // DB::table('model_has_roles')->where('model_id',$user->id)->delete();
-        // $user->assignRole($request->role);
+        DB::table('model_has_roles')->where('model_id',$user->id)->delete();
+        $user->assignRole($request->role);
         return redirect()->route('dashboard.user.index');
     }
 
