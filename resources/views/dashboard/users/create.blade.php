aaf6e45984398478a94a62174801a698bd88ca61
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-03-01 02:13:50 +0200

    Update create.blade.php

--- resources/views/dashboard/users/create.blade.php
+++ resources/views/dashboard/users/create.blade.php
@@ -3,67 +3,57 @@
 @section('content')
 
 <div class="card">
-    <div class="card-header">@lang('site.add')</div>
+    <div class="card-header">Edit User</div>
 
     <div class="card-body">
-        <form action="{{route('dashboard.user.store')}}" method="POST">
+        <form action="{{route('dashboard.user.update',$user)}}" method="POST">
             @csrf
+            @method('PUT')
             @include('partials.errors')
             <div class="form-group">
                 <label class="required" for="first_name">@lang('site.name')</label>
-                <input class="form-control" type="text" name="name"  id="name" required>
+                <input class="form-control" type="text" name="name" value="{{$user->name}}" id="name" required>
                 <span class="help-block"> </span>
             </div>
 
             <div class="form-group">
                 <label class="required" for="first_name">@lang('site.email')</label>
-                <input class="form-control" type="email" name="email"  id="name" required>
+                <input class="form-control" type="email" name="email" value="{{$user->email}}" id="name" required>
                 <span class="help-block"> </span>
             </div>
 
             <div class="form-group">
                 <label class="required" for="address">@lang('site.address')</label>
-                <input class="form-control" type="text" name="address"  id="address"  required>
+                <input class="form-control" type="text" name="address" <?php try{?>value="{{$user->profile->address}}"<?php }catch(\Exception $e){echo "\"";}?> id="address"  required>
                 <span class="help-block"> </span>
             </div>
 
             <div class="form-group">
                 <label class="required" for="phone_number">@lang('site.phone')</label>
-                <input class="form-control" type="text" name="phone" id="phone_number"  required>
+                <input class="form-control" type="text" name="phone" <?php try{?>value="{{$user->profile->phone}}"<?php }catch(\Exception $e){echo "\"";}?> id="phone_number"  required>
                 <span class="help-block"> </span>
             </div>
 
             <div class="form-group">
                 <label class="required" for="phone_number">@lang('site.contact')</label>
-                <input class="form-control" type="text" name="contact" id="phone_number"  required>
+                <input class="form-control" type="text" name="contact" <?php try{?>value="{{$user->profile->contact}}"<?php }catch(\Exception $e){echo "\"";}?> id="phone_number"  required>
                 <span class="help-block"> </span>
             </div>
 
             <div class="form-group">
                 <label for="client_id">Role</label>
                 <select class="form-control" name="role" id="project_id" required>
+
                     @foreach ($role as $item)
-                        <option value="{{ $item->id }}" {{ $item->id == 'id' ? 'selected' : '' }}>{{ $item->name }}
+                        <option value="{{ $item->id }}" {{ $userRole == $item->id ? 'selected' : '' }}>{{ $item->name }}
                         </option>
                     @endforeach
                 </select>
                 <span class="help-block"> </span>
             </div>
 
-            <div class="form-group">
-                <label class="required" for="new_password">Password</label>
-                <input class="form-control" type="password" name="password" id="new_password" required>
-                <span class="help-block"> </span>
-            </div>
-
-            <div class="form-group">
-                <label class="required" for="new_password_confirmation">Confirm Password</label>
-                <input class="form-control " type="password" name="confirmPassword" id="new_password_confirmation" required>
-                <span class="help-block"> </span>
-            </div>
-
             <button class="btn btn-primary" type="submit">
-                @lang('site.save')
+                Save
             </button>
         </form>
     </div>
